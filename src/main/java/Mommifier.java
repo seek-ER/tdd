b9b921da3c47ce6e79a1d84a0d240cfb9b44fa1f
Anson <1072476988@qq.com>
2020-09-14 18:15:33 +0800

    [lingzhen] refactor Mommifier.java.

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -2,38 +2,53 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Mommifier {
+    private final static double VOWEL_PERCENT = 0.3;
+    private final static String ADD_CONSTANT = "mommy";
+    int sum = 0;
+    StringBuilder result = new StringBuilder();
+
     public String convert(String inputString) {
         List<String> vowelList = Arrays.asList("a", "e", "i", "o", "u");
         String[] inputStringArray = inputString.split("");
 
-        int sum = 0;
-        StringBuilder result = new StringBuilder();
-
-        if (inputString.length() == 1){
-            if (vowelList.contains(inputString)){
-                return inputString+"mommy";
-            }
-            return inputString;
-        }else if(inputString.length() > 1){
-            for (String c : inputStringArray){
-                if (vowelList.contains(c)){
-                    sum += 1;
-                }
-            }
-            if ((float)sum/(float) inputStringArray.length >= 0.3){
-                for (String c : inputStringArray){
-                    if (vowelList.contains(c)){
-                        result.append(c).append("mommy");
-                    }else {
-                        result.append(c);
-                    }
-                }
+        if (inputString.length()>0){
+            sum = getVowelCountInString(inputStringArray,vowelList,sum);
+            boolean flag = judgeVowelPercentInString(sum,inputStringArray);
+            if (flag){
+                addMommy(inputStringArray, vowelList, result);
+                return result.toString();
             }else {
-                result = new StringBuilder(inputString);
+                return inputString;
             }
-            return result.toString();
         }else {
             return null;
+        }
+    }
+
+    private boolean isVowel(List vowelList, String c){
+        return vowelList.contains(c);
+    }
+
+    private int getVowelCountInString(String[] inputStringArray, List vowelList, int sum){
+        for (String inputChar : inputStringArray) {
+            if (isVowel(vowelList,inputChar)){
+                sum += 1;
+            }
+        }
+        return sum;
+    }
+
+    private boolean judgeVowelPercentInString(int sum,String[] inputStringArray ){
+        return (float)sum/(float) inputStringArray.length >= VOWEL_PERCENT;
+    }
+
+    private void addMommy(String[] inputStringArray, List vowelList, StringBuilder result){
+        for (String inputChar : inputStringArray){
+            if (isVowel(vowelList,inputChar)){
+                result.append(inputChar).append(ADD_CONSTANT);
+            }else {
+                result.append(inputChar);
+            }
         }
     }
 }
