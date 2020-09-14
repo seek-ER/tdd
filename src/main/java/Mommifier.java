8281977dd3c86f80f5e650c54024e9aa28dde5b9
Anson <1072476988@qq.com>
2020-09-14 16:43:56 +0800

    [lingzhen] return mommified result when given is a vowel char.

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Mommifier {
     public String convert(String inputString) {
+        List<String> vowelList = Arrays.asList("a", "e", "i", "o", "u");
+        if (vowelList.contains(inputString)){
+            return inputString+"mommy";
+        }
         return inputString;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -10,4 +10,12 @@ class MommifierTest {
         assertEquals(inputString,result);
     }
 
+    @Test
+    void should_return_mommified_result_when_mommifier_given_a_vowel(){
+        String inputString = "a";
+        Mommifier modifier = new Mommifier();
+        String result = modifier.convert(inputString);
+        assertEquals("amommy",result);
+    }
+
 }
